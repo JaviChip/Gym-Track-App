71d3475d3816d51b615afa1651e50d126f9f4908
javi <javi@ucr-secure-22-10-13-137-120.wnet.ucr.edu>
2023-04-29 18:30:12 -0700

    Initial commit

---
+++ gym-track/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderContent from './HeaderContent';
+
+const Header = () => {
+  return (
+    <div>
+      <HeaderContent />
+    </div>
+  );
+};
+
+export default Header;
---
+++ gym-track/src/components/HeaderContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const HeaderContent = () => {
+  return (
+    <div className="header">
+      <h1 className="header-text">Today's Workout</h1>
+    </div>
+  );
+};
+
+export default HeaderContent;
